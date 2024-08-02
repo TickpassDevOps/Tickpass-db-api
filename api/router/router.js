ef4b2080815c58f956fb797b3a048f2f0fb93f29
TickpassDev <TickpassDev@gmail.com>
2024-08-02 11:01:33 -0300

    Update router.js

--- api/router/router.js
+++ api/router/router.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 
-const peticiones = require('./querys');
+const peticiones = require('../querys');
 
 router.get("/", peticiones.get);
 router.post("/ingresar",peticiones.post);
